36fafd2c175004d6ffc883ebf70dd3291297ce7f
LuanSALima <luanlimadev@gmail.com>
2021-09-07 14:10:50 -0300

    Configured Babel

--- backend/src/Tests/FirstTest.spec.ts
+++ backend/src/Tests/FirstTest.spec.ts
@@ -1,3 +1,11 @@
+import { User } from '@models/User';
+
 it('should pass because 1+1 = 2', () => {
 	expect(1+1).toBe(2);
-})
+});
+
+test('if user.name receives the correct name', () => {
+	const user = new User('Teste', 'teste@gmail.com');
+	expect(user.name).toEqual('Teste');
+	expect(user.email).toEqual('teste@gmail.com')
+});
